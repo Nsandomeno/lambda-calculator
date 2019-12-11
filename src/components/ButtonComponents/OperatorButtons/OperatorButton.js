6d95829e05285eab78aacc874fbbb1e57074b986
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2019-12-11 10:16:30 -0500

    Some more styling.

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -4,9 +4,8 @@ import {operators} from "../../../data.js";
 export const OperatorButton = (props) => {
   //console.log("OpButton Props:", props);
   return (
-    <div id="opContainer" className="op-div">
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+
       <button className="op-btn" onClick={() => props.setDisplayVal(props.displayVal !== "" ? props.displayVal + props.op : null)}>{props.op}</button>
-    </div>
+   
   );
 };
